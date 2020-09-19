b66b5d19897b4591f82f70d73951c4ad35e6183e
yamayamaKo <yamachanwow@gmail.com>
2020-09-19 18:28:43 +0900

    静的ファイルを導入できるようにした

--- server.py
+++ server.py
@@ -1,10 +1,24 @@
-from bottle import route, run, request, template
+from bottle import route, run, request, template, static_file, url
 import ergoscore_learn
 import os
+def make_response(input_min,input_sec,input_minsec,event):
+    if(event=="twenty"):
+        return str(input_min)
+    
+    response = ""
+    if(input_sec<10):
+        response += str(input_min) + ":0" + str(input_sec) + "." + str(input_minsec)
+    else:
+        response += str(input_min) + ":" + str(input_sec) + "." + str(input_minsec)
+    return response
+    
+@route("/static/<filepath:path>",name="static_file")
+def static(filepath):
+    return static_file(filepath,root="./static")
 
 @route("/pred")
 def hello():
-    return template("index.html", text="")
+    return template("index.html", input_text="", text="")
 
 @route("/pred", method="POST")
 def do_predict():
@@ -13,9 +27,10 @@ def do_predict():
     input_min = int(request.forms.input_min)
     input_sec = int(request.forms.input_sec)
     input_minsec = int(request.forms.input_minsec)
+    input_text = make_response(input_min,input_sec,input_minsec,own)
     response_text = ergoscore_learn.predict(own,pred,input_min,input_sec,input_minsec)
 
-    return template("index",text=response_text)
+    return template("index",input_text=input_text,text=response_text)
     
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-#run(host="localhost",port=8080,debug=True)
+#run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(host="localhost",port=8080,debug=True)
